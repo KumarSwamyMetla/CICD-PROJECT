b94b768ae724db56e18bcb2cc45c2d051c80bcab
KUMAR <kumar@KUMARs-MacBook-Air.local>
2025-09-23 20:13:46 +0530

    Fourth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/CICD-PROJECT/', // Add this line with your repo name
     plugins: [react()],
     server: {
         host: '0.0.0.0', // bind to all interfaces
