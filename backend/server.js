f7c1640c8e114f5410bb6b1b73722112b188ae5f
KUMAR <kumar@KUMARs-MacBook-Air.local>
2025-09-25 00:41:27 +0530

    Restore full project from zip

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
 
 app.get("/", (req, res) => {
-  res.send("API Working")
+    res.send("API Working")
 });
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
